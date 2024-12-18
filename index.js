06fb2683ef1cce5ee4abad4d9ec5d61a9465addf
trimikpro <vupotiroy112@gmail.com>
2024-12-18 18:14:32 +0600

    job portal jobsCollection

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors());
@@ -30,6 +30,29 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // jobs related apis
+        const jobsCollection = client.db('jobPortal').collection('jobs')
+
+        
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await jobsCollection.findOne(query);
+            res.send(result);
+        }) 
+
+
+
+
+
+
     } finally {
         // Ensures that the client will close when you finish/error
        // await client.close();
